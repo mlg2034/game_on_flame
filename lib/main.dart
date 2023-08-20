3a6d6ebb535bc48b3c047e4a57e493bd1c68947a
mlg2034 <ashatkabytaev@gmail.com>
2023-08-20 21:08:05 +0600

    app init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  final game = FlameGame();
+  runApp(GameWidget(game: game));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
